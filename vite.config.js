981692887d9b90e9bd58c6271243252105506e80
Gabriel Gomez <luisggm33@gmail.com>
2026-03-01 15:36:28 -0600

    Add preview configuration to Vite for allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  preview: {
+    allowedHosts: ['pawstore-production.up.railway.app'],
+  },
 })
